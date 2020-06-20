f6d280c1b7c856c05ba7f94749e782257e3e4b2f
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2020-06-20 09:49:49 -0400

    Remove fork helper script (app has built-in tools)

--- plugins/functions/fork.zsh
+++
@@ -1,3 +0,0 @@
-function fork {
-    open -a "Fork" $argv
-}
--- plugins/functions/functions.plugin.zsh
+++ plugins/functions/functions.plugin.zsh
@@ -3,7 +3,6 @@ BIN=$(dirname $0)
 source "${BIN}/azure-devops-pull-latest.zsh"
 source "${BIN}/brew-depend.zsh"
 source "${BIN}/code.zsh"
-source "${BIN}/fork.zsh"
 source "${BIN}/image-size.zsh"
 source "${BIN}/install-dotfiles.zsh"
 source "${BIN}/install-shell-addons.zsh"
