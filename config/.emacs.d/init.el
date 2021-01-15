81bdb794abe4674db478f98b6309dac45e949939
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2021-01-15 07:18:06 -0500

    MELPA Stable to MELPA (themes)

--- config/.emacs.d/init.el
+++ config/.emacs.d/init.el
@@ -1,4 +1,3 @@
 
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
