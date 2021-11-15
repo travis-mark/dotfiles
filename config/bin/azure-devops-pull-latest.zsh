0ad1c0fb64e05a74e7126663a6db5e8c6e4b3a79
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2021-11-15 15:21:48 -0500

    Server change

--- config/bin/azure-devops-pull-latest.zsh
+++ config/bin/azure-devops-pull-latest.zsh
@@ -2,11 +2,12 @@
 
 #az login > /dev/null
 az extension add --name azure-devops
-ORGANIZATIONS="penndotvso.visualstudio.com"
+ORGANIZATIONS="penndotvso"
 for ORGANIZATION in $ORGANIZATIONS; do
-    PROJECTS=`az devops project list --organization "https://$ORGANIZATION" | jq '.value|.[]|.name' --raw-output`
+    URL="https://dev.azure.com/$ORGANIZATION"
+    PROJECTS=`az devops project list --organization ${URL} | jq '.value|.[]|.name' --raw-output`
     for PROJECT ($=PROJECTS); do
-        REPOS=`az repos list --organization "https://$ORGANIZATION" --project $PROJECT | jq '.[].sshUrl' --raw-output`
+        REPOS=`az repos list --organization ${URL} --project $PROJECT | jq '.[].sshUrl' --raw-output`
         for REPO ($=REPOS); do
             DIR=$(echo ${REPO} | awk 'BEGIN { FS = "/" } ; {print $(NF)}')
             echo "\e[0;32m${DIR} \e[m" 
