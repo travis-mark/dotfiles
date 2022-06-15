5795b9496b9ae1b56000e21b064fb2199b6da27c
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2022-06-14 21:21:39 -0400

    org GTD

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -96,9 +96,71 @@ shell."
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 
 ;; Org mode
+;; Original: https://www.labri.fr/perso/nrougier/GTD/index.html
 (require 'org)
-(setq org-agenda-files (list "~/Documents/journal/" "~/Documents/notes/" "~/Documents/work/"))
-(add-to-list 'org-link-frame-setup '(file . find-file))  ;; Open in same window
+;; (setq org-agenda-files (list "~/Documents/journal/" "~/Documents/notes/" "~/Documents/work/")) 
+;; (add-to-list 'org-link-frame-setup '(file . find-file))  ;; Open in same window
+(setq org-directory "~/Documents/org")
+(setq org-agenda-files (list "~/Documents/org/inbox.org" "~/Documents/org/agenda.org"))
+(setq org-capture-templates
+      `(("i" "Inbox" entry  (file "inbox.org") 
+         ,(concat "* TODO %?\n"
+                 "/Entered on/ %U"))
+        ("m" "Meeting" entry  (file+headline "agenda.org" "Future")
+         ,(concat "* %? :meeting:\n"
+                  "<%<%Y-%m-%d %a %H:00>>"))
+        ("n" "Note" entry  (file "notes.org")
+         ,(concat "* Note (%a)\n"
+                  "/Entered on/ %U\n" "  %?"))))
+(define-key global-map (kbd "C-c c") 'org-capture)
+(defun org-capture-inbox ()
+     (interactive)
+     (call-interactively 'org-store-link)
+     (org-capture nil "i"))
+(define-key global-map (kbd "C-c i") 'org-capture-inbox)
+(define-key global-map (kbd "C-c a") 'org-agenda)
+(setq org-agenda-hide-tags-regexp ".") ;; Hide tags
+(setq org-agenda-prefix-format
+      '((agenda . " %i %-12:c%?-12t% s")
+        (todo   . " ")
+        (tags   . " %i %-12:c")
+        (search . " %i %-12:c")))
+(setq org-refile-targets
+      '(("projects.org" :regexp . "\\(?:\\(?:Note\\|Task\\)s\\)")))
+(setq org-refile-use-outline-path 'file)
+(setq org-outline-path-complete-in-steps nil)
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "NEXT(n)" "HOLD(h)" "|" "DONE(d)")))
+(defun log-todo-next-creation-date (&rest ignore)
+  "Log NEXT creation time in the property drawer under the key 'ACTIVATED'"
+  (when (and (string= (org-get-todo-state) "NEXT")
+             (not (org-entry-get nil "ACTIVATED")))
+    (org-entry-put nil "ACTIVATED" (format-time-string "[%Y-%m-%d]"))))
+(add-hook 'org-after-todo-state-change-hook #'log-todo-next-creation-date)
+(setq org-agenda-custom-commands
+      '(("g" "Get Things Done (GTD)"
+         ((agenda ""
+                  ((org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'deadline))
+                   (org-deadline-warning-days 0)))
+          (todo "NEXT"
+                ((org-agenda-skip-function
+                  '(org-agenda-skip-entry-if 'deadline))
+                 (org-agenda-prefix-format "  %i %-12:c [%e] ")
+                 (org-agenda-overriding-header "\nTasks\n")))
+          (agenda nil
+                  ((org-agenda-entry-types '(:deadline))
+                   (org-agenda-format-date "")
+                   (org-deadline-warning-days 7)
+                   (org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'notregexp "\\* NEXT"))
+                   (org-agenda-overriding-header "\nDeadlines")))
+          (tags-todo "inbox"
+                     ((org-agenda-prefix-format "  %?-12t% s")
+                      (org-agenda-overriding-header "\nInbox\n")))
+          (tags "CLOSED>=\"<today>\""
+                ((org-agenda-overriding-header "\nCompleted today\n")))))))
+(setq org-log-done 'time)
 
 ;; Python
 (setq python-indent-offset 4)
