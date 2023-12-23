5430ab7a5e2a53a50deefd9fe7cfbbfc480169e4
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-12-23 11:25:56 -0500

    Misc cleanup

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is a collection of my miscellaneous scripts and customizations.
 
 ## Running Scripts
-If you're trying to run something, you'll to want the following above and beyond standard UNIX and Perl/Python/Ruby/Shell.
+If you're trying to run something, you'll to want the following above and beyond standard UNIX and Perl/Python/Ruby/Shell:
 
 - [fzf](https://github.com/junegunn/fzf)
 - [ImageMagick](https://github.com/ImageMagick/ImageMagick)
--- bin/wordle-dl.sh
+++ bin/wordle-dl.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+ANSWERS_FOLDER="/tmp/$$"
+ANSWERS_FILE="${ANSWERS_FOLDER}/answers.txt"
 START="2021-06-19"
-rm answers.txt
+
+mkdir -p ${ANSWERS_FOLDER}
 for i in $(seq 0 1000); do 
     d=`date -j -v+${i}d -f %Y-%m-%d ${START} +%Y-%m-%d`
-    curl "https://www.nytimes.com/svc/wordle/v2/${d}.json" >> answers.txt
-    echo >> answers.txt
+    curl "https://www.nytimes.com/svc/wordle/v2/${d}.json" >> ${ANSWERS_FILE}
+    echo >> ${ANSWERS_FILE}
 done
+
+open ${ANSWERS_FILE}
