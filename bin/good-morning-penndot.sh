54c9f6ee485d1eaa09f3892ff141a7e588f9dda4
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2025-04-22 17:51:39 -0400

    Startup and shutdown scripts

---
+++ bin/good-morning-penndot.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+ssh-add ${HOME}/.ssh/id_penndot_c_tralucke
+
+open /Applications/Microsoft\ Outlook.app 
+open /Applications/Microsoft\ Teams.app 
+open ${CODE}/penndotvso/ConstructionAAR/ConstructionAAR.xcodeproj
+fork ${CODE}/penndotvso/ConstructionAAR/
+open /Applications/Microsoft\ OneNote.app 
+open /Applications/Emacs.app 
+open ${HOME}/Library/CloudStorage/OneDrive-CommonwealthofPennsylvania/
+open /Applications/Microsoft\ Edge.app 
+open ${HOME}/Library/CloudStorage/OneDrive-CommonwealthofPennsylvania/Documents/PSS.xlsx
---
+++ bin/good-night-penndot.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+ssh-add -d ${HOME}/.ssh/id_penndot_c_tralucke
+
+killall "Microsoft Outlook"
+killall "Microsoft Teams"
+killall "Xcode"
+killall "Simulator"
+killall "Fork"
+killall "Microsoft OneNote"
+killall "Microsoft Edge"
+killall "Microsoft Excel"
+killall "Proxyman"
